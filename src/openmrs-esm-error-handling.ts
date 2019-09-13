9c9eeb9535fcda453c681cea8f2db7baff262ae8
Joel Denning <joeldenning@gmail.com>
2019-09-13 13:14:45 -0600

    Implementing handleApiError (#1)

--- src/openmrs-esm-error-handling.ts
+++ src/openmrs-esm-error-handling.ts
@@ -1,2 +1,13 @@
-// eslint-disable-next-line
-console.log("hello world");
+// import { showToast } from '@openmrs/esm-styleguide'
+
+export function handleApiError() {
+  return incomingResponseErr => {
+    setTimeout(() => {
+      throw incomingResponseErr;
+    });
+  };
+}
+
+window.addEventListener("error", info => {
+  // call "showToast()" or similar inside of openmrs-esm-styleguide
+});
